8ee83de0700ba48b43b4b5d8557399f595c79ce8
christanfox <bayshorestinks@aol.com>
2022-05-01 19:29:48 -0400

    Setup thoughts-routes.js file

--- routes/api/thoughts-routes.js
+++ routes/api/thoughts-routes.js
@@ -1,3 +1,24 @@
 const router = require('express').Router();
 
+const {
+    getAllThought,
+    findThoughtById,
+    createThought,
+    updateThought,
+    deleteThought
+} = require('../../controllers/thought-controller');
+
+// Setup GET all and POST in /api/thoughts
+router
+    .route('/')
+    .get(getAllThought)
+    .post(createThought);
+
+// Setup GET one, PUT, and DELETE in /api/thoughts/:id
+router
+    .route('/:id')
+    .get(findThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
 module.exports = router;
