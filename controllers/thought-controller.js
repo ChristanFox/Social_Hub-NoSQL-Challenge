a45c582babd0e6a8fe0c7532d488314893d7bd89
christanfox <bayshorestinks@aol.com>
2022-05-08 12:09:14 -0400

    Added Create and Delete Reaction functionality

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -75,7 +75,38 @@ const thoughtController = {
                 }
                 res.json(dbUserData);
             }).catch(err => res.json(err));
-        }
+        },
+
+    createReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $push: { reactions: body }},
+            { new: true, runValidators: true }
+        )
+        .populate({ path: 'reactions', select: '-__v'})
+        .select('-__v')
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'Invalid ID!'});
+                return;
+            }
+            res.json(dbThoughtData);
+        }).catch(err => res.status(400).json(err))
+    },
+    
+    deleteReaction({ params }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $pull: { reactions: { reactionId: params.reactionId } }},
+            { new: true }
+        ).then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'Invalid ID!'});
+                return;
+            }
+            res.json(dbThoughtData);
+        }).catch(err => res.json(err));
+    }
 };
 
 module.exports = thoughtController
